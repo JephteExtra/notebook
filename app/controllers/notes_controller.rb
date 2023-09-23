0a5ba5b5479f44e7e6c1f7823a73ae7689d05b6e
Jephte Extra <jephteextra@gmail.com>
2023-09-22 23:37:35 -0400

    Add notes vreate action. Change field contents into content in note model

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,13 +7,29 @@ class NotesController < ApplicationController
   def show
   end
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to note_url(@note), notice: "Note was created successfully" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_note
     @note = Note.find(params[:id])
   end
 
-  def new
-    @note = Note.new
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
+  
 end
