fdcde31fb61731ba8fcaaf4b69eecebaa605f975
Jephte Extra <jephteextra@gmail.com>
2023-09-22 22:44:28 -0400

    Add note show page and controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,15 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: %i[show]
   def index
     @notes = Note.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
